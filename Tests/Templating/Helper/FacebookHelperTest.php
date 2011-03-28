62b3148e6381efdb7233597cbb84e0b55b24e477
excelwebzone <hagaiarieli@Michael-H-Arielis-MacBook-Pro.local>
2011-03-28 10:20:05 -0700

    Updated for shorter bundle names

--- Tests/Templating/Helper/FacebookHelperTest.php
+++ Tests/Templating/Helper/FacebookHelperTest.php
@@ -19,7 +19,7 @@ class FacebookHelperTest extends \PHPUnit_Framework_TestCase
         $templating
             ->expects($this->once())
             ->method('render')
-            ->with('AuthBundle::initialize.php', array(
+            ->with('EWZAuth::facebook.html.php', array(
                 'appId'   => 123,
                 'cookie'  => false,
                 'culture' => 'en_US',
